5936913bbbe02e65564ac061633f756e87baac14
Kazuki Shimamoto <jakky0529@gmail.com>
2022-01-20 14:44:59 +0900

    クイズを3問ランダムに返す

--- app/controllers/api/v1/quiz_controller.rb
+++ app/controllers/api/v1/quiz_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class QuizController < ApplicationController
       def index
-        quiz = Quiz.where(level: params['level'])
+        quiz = Quiz.where(level: params['level'] || 'easy').shuffle.first(3)
 
         render json: quiz, status: :ok
       end
